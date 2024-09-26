065132eb68a5106eb83d5e750bda4f3c7474a4bb
ihad01 <maxjihad59@gmail.com>
2024-09-26 19:33:55 +0600

    Update intro.js

--- scripts/commands/intro.js
+++ scripts/commands/intro.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("Intro")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("-Intro")==0) {
 		var msg = {
 				body: "𝐒𝐄𝐈𝐊𝐇 𝐋𝐈𝐊𝐇𝐎𝐍 𝐀𝐇𝐌𝐄𝐃",
 				attachment: fs.createReadStream(__dirname + `/Nayan/Intro.mp3`)
