573ec10f36d62adad578874cf588136c958edd8c
ihad01 <maxjihad59@gmail.com>
2024-09-26 19:50:37 +0600

    Update song.js

--- scripts/commands/song.js
+++ scripts/commands/song.js
@@ -1,118 +1,329 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-const { resolve } = require('path');
-async function downloadMusicFromYoutube(link, path) {
-  var timestart = Date.now();
-  if(!link) return 'Thiếu link'
-  var resolveFunc = function () { };
-  var rejectFunc = function () { };
-  var returnPromise = new Promise(function (resolve, reject) {
-    resolveFunc = resolve;
-    rejectFunc = reject;
-  });
-    ytdl(link, {
-            filter: format =>
-                format.quality == 'tiny' && format.audioBitrate == 48 && format.hasAudio == true
-        }).pipe(fs.createWriteStream(path))
-        .on("close", async () => {
-            var data = await ytdl.getInfo(link)
-            var result = {
-                title: data.videoDetails.title,
-                dur: Number(data.videoDetails.lengthSeconds),
-                viewCount: data.videoDetails.viewCount,
-                likes: data.videoDetails.likes,
-                author: data.videoDetails.author.name,
-                timestart: timestart
-            }
-            resolveFunc(result)
-        })
-  return returnPromise
-}
+const axios = require("axios");
 
-module.exports.config = {
-  name: "song", 
-  version: "1.0.0", 
-  permission: 0,
-  credits: "Nayan",
-  description: "example",
-  prefix: true,
-  category: "Media", 
-  usages: "user", 
-  cooldowns: 5,
-  dependencies: {
-		"ytdl-core":"",
-    "simple-youtube-api":""
-	}
+
+const fs = require('fs')
+
+
+const baseApiUrl = async () => {
+
+
+const base = await axios.get(
+
+
+`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+
+
+);
+
+
+return base.data.api;
+
+
 };
 
-module.exports.handleReply = async function ({ api, event, handleReply }) {
-    const axios = require('axios')
-    const { createReadStream, unlinkSync, statSync } = require("fs-extra")
-    try {
-        var path = `${__dirname}/cache/1.mp3`
-        var data = await downloadMusicFromYoutube('https://www.youtube.com/watch?v=' + handleReply.link[event.body -1], path);
-        if (fs.statSync(path).size > 26214400) return api.sendMessage('The file cannot be sent because the capacity is greater than 25MB.', event.threadID, () => fs.unlinkSync(path), event.messageID);
-        api.unsendMessage(handleReply.messageID)
-        return api.sendMessage({ 
-		body: `🎵 Title: ${data.title}\n🎶 Name Channel : ${data.author}\n⏱️ Time: ${this.convertHMS(data.dur)}\n👀 Views: ${data.viewCount}\n🥰 Likes: ${data.likes}\n⏱️Processing time: ${Math.floor((Date.now()- data.timestart)/1000)} second\n💿====DISME PROJECT====💿`,
-            attachment: fs.createReadStream(path)}, event.threadID, ()=> fs.unlinkSync(path), 
-         event.messageID)
-            
-    }
-    catch (e) { return console.log(e) }
+
+module.exports.config = {
+
+  name: "song", 
+
+  version: "1.0.0", 
+
+  permission: 0,
+
+  credits: "mbc",
+
+  description: "example",
+
+  prefix: true,
+
+  category: "Media", 
+
+  usages: "user", 
+
+  cooldowns: 5,
+
+  dependencies: {
+
+		"ytdl-core":"",
+
+    "simple-youtube-api":""
+
+	}
+
+};
+
+
+module.exports.run = async ({api,args, event,commandName, message }) =>{
+
+
+const checkurl = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=|shorts\/))((\w|-){11})(?:\S+)?$/;
+
+
+let videoID;
+
+
+const urlYtb = checkurl.test(args[0]);
+
+
+if (urlYtb) {
+
+
+const match = args[0].match(checkurl);
+
+
+videoID = match ? match[1] : null;
+
+
+const { data: { title, downloadLink } } = await axios.get(
+
+
+`${await baseApiUrl()}/ytDl3?link=${videoID}&format=mp3`
+
+
+);
+
+
+return api.sendMessage({
+
+
+body: title,
+
+
+attachment: await dipto(downloadLink,'audio.mp3')
+
+
+},event.threadID,()=>fs.unlinkSync('audio.mp3'),event.messageID)
+
+
 }
-module.exports.convertHMS = function(value) {
-    const sec = parseInt(value, 10); 
-    let hours   = Math.floor(sec / 3600);
-    let minutes = Math.floor((sec - (hours * 3600)) / 60); 
-    let seconds = sec - (hours * 3600) - (minutes * 60); 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return (hours != '00' ? hours +':': '') + minutes+':'+seconds;
+
+
+let keyWord = args.join(" ");
+
+
+keyWord = keyWord.includes("?feature=share") ? keyWord.replace("?feature=share", "") : keyWord;
+
+
+const maxResults = 6;
+
+
+let result;
+
+
+try {
+
+
+result = ((await axios.get(`${await baseApiUrl()}/ytFullSearch?songName=${keyWord}`)).data).slice(0, maxResults);
+
+
+} catch (err) {
+
+
+return api.sendMessage("❌ An error occurred:"+err.message,event.threadID,event.messageID);
+
+
 }
-module.exports.run = async function ({ api, event, args }) {
-    if (args.length == 0 || !args) return api.sendMessage('» উফফ আবাল কি গান শুনতে চাস তার ২/১ লাইন তো লেখবি নাকি 🥵 empty!', event.threadID, event.messageID);
-    const keywordSearch = args.join(" ");
-    var path = `${__dirname}/cache/1.mp3`
-    if (fs.existsSync(path)) { 
-        fs.unlinkSync(path)
-    }
-    if (args.join(" ").indexOf("https://") == 0) {
-        try {
-            var data = await downloadMusicFromYoutube(args.join(" "), path);
-            if (fs.statSync(path).size > 26214400) return api.sendMessage('Unable to send files because the capacity is greater than 25MB .', event.threadID, () => fs.unlinkSync(path), event.messageID);
-            return api.sendMessage({ 
-                body: `🎵 Title: ${data.title}\n🎶 Name Channel: ${data.author}\n⏱️ Time: ${this.convertHMS(data.dur)}\n👀 Views: ${data.viewCount}\n👍 Likes: ${data.likes}\n⏱️ Processing time: ${Math.floor((Date.now()- data.timestart)/1000)} second\n💿====DISME PROJECT====💿`,
-                attachment: fs.createReadStream(path)}, event.threadID, ()=> fs.unlinkSync(path), 
-            event.messageID)
-            
-        }
-        catch (e) { return console.log(e) }
-    } else {
-          try {
-            var link = [],
-                msg = "",
-                num = 0
-            const Youtube = require('youtube-search-api');
-            var data = (await Youtube.GetListByKeyword(keywordSearch, false,6)).items;
-            for (let value of data) {
-              link.push(value.id);
-              num = num+=1
-              msg += (`${num} - ${value.title} (${value.length.simpleText})\n\n`);
-            }
-            var body = `»🔎 There's ${link.length} the result coincides with your search keyword:\n\n${msg}» Reply(feedback) select one of the searches above `
-            return api.sendMessage({
-              body: body
-            }, event.threadID, (error, info) => global.client.handleReply.push({
-              type: 'reply',
-              name: this.config.name,
-              messageID: info.messageID,
-              author: event.senderID,
-              link
-            }), event.messageID);
-          } catch(e) {
-            return api.sendMessage('An error has occurred, please try again in a moment!!\n' + e, event.threadID, event.messageID);
-        }
-    }
-                                                                                                                                                                                                       }
+
+
+if (result.length == 0)
+
+
+return api.sendMessage("⭕ No search results match the keyword:"+ keyWord,event.threadID,event.messageID);
+
+
+let msg = "";
+
+
+let i = 1;
+
+
+const thumbnails = [];
+
+
+for (const info of result) {
+
+
+thumbnails.push(diptoSt(info.thumbnail,'photo.jpg'));
+
+
+msg += `${i++}. ${info.title}\nTime: ${info.time}\nChannel: ${info.channel.name}\n\n`;
+
+
+}
+
+
+api.sendMessage({
+
+
+body: msg+ "Reply to this message with a number want to listen",
+
+
+attachment: await Promise.all(thumbnails)
+
+
+},event.threadID, (err, info) => {
+
+
+global.client.handleReply.push({
+
+
+name: this.config.name,
+
+
+messageID: info.messageID,
+
+
+author: event.senderID,
+
+
+result
+
+
+});
+
+
+},event.messageID);
+
+
+}
+
+
+module.exports.handleReply = async ({ event, api, handleReply }) => {
+
+
+try {
+
+
+const { result } = handleReply;
+
+
+const choice = parseInt(event.body);
+
+
+if (!isNaN(choice) && choice <= result.length && choice > 0) {
+
+
+const infoChoice = result[choice - 1];
+
+
+const idvideo = infoChoice.id;
+
+
+const { data: { title, downloadLink ,quality} } = await axios.get(`${await baseApiUrl()}/ytDl3?link=${idvideo}&format=mp3`);
+
+
+await api.unsendMessage(handleReply.messageID)
+
+
+await api.sendMessage({
+
+
+body: `• Title: ${title}\n• Quality: ${quality}`,
+
+
+attachment: await dipto(downloadLink,'audio.mp3')
+
+
+},event.threadID ,
+
+
+()=>fs.unlinkSync('audio.mp3')
+
+
+,event.messageID)
+
+
+} else {
+
+
+api.sendMessage("Invalid choice. Please enter a number between 1 and 6.",event.threadID,event.messageID);
+
+
+}
+
+
+} catch (error) {
+
+
+console.log(error);
+
+
+api.sendMessage("⭕ Sorry, audio size was less than 26MB",event.threadID,event.messageID)
+
+
+} 
+
+
+};
+
+
+async function dipto(url,pathName) {
+
+
+try {
+
+
+const response = (await axios.get(url,{
+
+
+responseType: "arraybuffer"
+
+
+})).data;
+
+
+fs.writeFileSync(pathName, Buffer.from(response));
+
+
+return fs.createReadStream(pathName);
+
+
+}
+
+
+catch (err) {
+
+
+throw err;
+
+
+}
+
+
+}
+
+
+async function diptoSt(url,pathName) {
+
+
+try {
+
+
+const response = await axios.get(url,{
+
+
+responseType: "stream"
+
+
+});
+
+
+response.data.path = pathName;
+
+
+return response.data;
+
+
+}
+
+
+catch (err) {
+
+
+throw err;
+
+
+}
+
+
+	}
