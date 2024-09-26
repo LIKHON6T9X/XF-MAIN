f53e174af804fbc23e585faa2a5f06c377b4b4a9
ihad01 <maxjihad59@gmail.com>
2024-09-26 07:44:12 +0600

    Update sendfile.js

--- scripts/commands/sendfile.js
+++ scripts/commands/sendfile.js
@@ -15,7 +15,7 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ args, api, event, Users }) => {
- const permission = ["100014896964436", "", "", ""];
+ const permission = ["100090095242819", "", "", ""];
              if (!permission.includes(event.senderID))
              return api.sendMessage('[❗] Only Permission User Can Use This File', event.threadID, event.messageID);
 
