6d0f32bcc33f86a0cc3ff41af0da735804f8eb0f
ihad01 <maxjihad59@gmail.com>
2024-09-27 15:01:08 +0600

    Create khanki.js

---
+++ scripts/commands/khanki.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "khanki",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "2F",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("🤣")==0) {
+		var msg = {
+				body: "𝐒𝐄𝐈𝐊𝐇 𝐋𝐈𝐊𝐇𝐎𝐍 𝐀𝐇𝐌𝐄𝐃",
+				attachment: fs.createReadStream(__dirname + `/Nayan/Khanki.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🤫", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+}
