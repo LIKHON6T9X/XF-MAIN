adeb7a26b7ea0fc936a641b770e6d52dfe1638ea
ihad01 <maxjihad59@gmail.com>
2024-09-27 15:20:51 +0600

    Create intro.js

---
+++ scripts/commands/intro.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "intro",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "2F",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("☺️")==0) {
+		var msg = {
+				body: "𝐒𝐄𝐈𝐊𝐇 𝐋𝐈𝐊𝐇𝐎𝐍 𝐀𝐇𝐌𝐄𝐃",
+				attachment: fs.createReadStream(__dirname + `/Nayan/Intro.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+}
