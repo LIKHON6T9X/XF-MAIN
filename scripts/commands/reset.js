338ab43572222e53c4d771bb8c49ba5ae4dcaa02
ihad01 <maxjihad59@gmail.com>
2024-09-28 21:50:41 +0600

    Update reset.js

--- scripts/commands/reset.js
+++ scripts/commands/reset.js
@@ -13,13 +13,13 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("987")==0 || (event.body.indexOf("Reset bot")==0 || (event.body.indexOf("Reset")==0 || (event.body.indexOf("reset")==0)))) {
+	if (event.body.indexOf("987")==0 || (event.body.indexOf(" ")==0 || (event.body.indexOf("Reset")==0 || (event.body.indexOf(" ")==0)))) {
 		var msg = {
-				body: "𝙱𝙾𝚃 𝚂𝚃𝙰𝚁𝚃 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚢. 𝚃𝚢𝙿𝙴. 𝚑𝚎𝚕𝚙 𝙰𝙽𝙳 𝚂𝙴𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 𝙻𝙸𝚂𝚃 🔰✅"
+				body: "╭──────────────────╮\n🌺 𝙱𝙾𝚃 𝚁𝙴𝚂𝙴𝚃 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻🌺\n╰──────────────────╯"
     }
 			api.sendMessage(msg, threadID, messageID);
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-}
+	}
